99a0acc527b993a6a5bd99d801f85297abdca34e
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2019-09-15 12:59:50 -0400

    Added login form
    Added a login form, has not been hooked up to a backend to enable logging in. Also created a quick loop for posts on the dashboard once they are enabled.

--- Flask.py
+++ Flask.py
@@ -1,13 +1,26 @@
 #Various imports
 from flask import Flask, flash, render_template
-
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 #Initial screen upon entering website.
 @app.route("/")
 def index():
-    return render_template('HomePage/index.html')
+    user = {'username': 'Larry'}
+    posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
+    return render_template('HomePage/index.html', user = user, posts = posts)
 
-
+@app.route('/login')
+def login():
+    return render_template('Login/login.html')
 #Run
 if __name__ == "__main__":
 
