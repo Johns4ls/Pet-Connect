e666070b96a3c6fec09ee7a8534ac83a422e9775
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2019-10-03 21:27:57 -0400

    Added routes for dog creation
    Added route to create a family, added route to join a family, added route to create a dog. Also added a few comments, and some white space.

--- Flask.py
+++ Flask.py
@@ -22,7 +22,13 @@ def load_user(userid):
 @login_required
 def index():
 
-    #Temporary filler data
+    '''Will be here soon! Need to add the 
+    ability to create new posts on the Dashboard
+    once dogs are made, since Users are now
+    able to hit the Dashboard. This will require
+    JavaScript to submit the request without redirecting.
+    The posts submits will be there own separate route.
+    Much like likes will.'''
     user = {'username': 'Larry'}
     posts = [
     {
@@ -52,6 +58,8 @@ def login():
             return redirect('/dashboard')
         flash("Email or Password is incorrect.")
     return render_template('Login/login.html', Registerform=Registerform, Loginform=Loginform)
+
+#Start registration on landing page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     Registerform = RegisterForm()
@@ -66,6 +74,8 @@ def register():
         return redirect('/')
         
     return render_template('Login/login.html', Registerform=Registerform, Loginform=Loginform)
+
+#Create a new User
 @app.route('/userInfo', methods=['GET', 'POST'])
 def userInfo():
     UserInfoform = UserInfoForm()
@@ -80,12 +90,65 @@ def userInfo():
     Tlbx.new_Account(firstName, lastName, email, password, address, city, state, zipCode)
     Tlbx.loginUser(email)
     return redirect('/dashboard')
+
 @app.route('/forgotPass', methods=['GET', 'POST'])
 def forgotPass():
     return render_template('PasswordReset/PasswordReset.html')
+
 @app.route('/PasswordReset', methods=['GET','POST'])
 def passwordReset():
     print("WIP")
+
+@app.route('/FamilyCreation', methods=['GET','POST'])
+@login_required
+def familyCreation():
+    print("Create a new Family.")
+
+@app.route('/JoinFamily', methods=['GET','POST'])
+@login_required
+def JoinFamily():
+    print("Join an Existing family.")
+
+@app.route('/DogCreation', methods=['GET','POST'])
+@login_required
+def DogCreation():
+    '''We may want to split these into functions...
+    If we let users have the ability to update these items
+    it may be easier to just call a function from tlbx.'''
+    #Query for breed existence in DB here
+    if (breedID is None)
+        cur, db = dbConnectDict()
+        breedQuery = "INSERT INTO tbreed (breed) VALUES (%s);"
+        data = (breed)
+        cur.execute(breedQuery, data)
+        db.commit()
+        breedID = cur.insert_id()
+    #Query for favorite park existence in DB here
+    if (favParkID is None)
+        image = None
+        addressQuery = "INSERT INTO tAddress (address, city, state, zip) VALUES (%s, %s, %s, %s);"
+        data = (address, city, state zipCode)
+        cur.execute(addressQuery, data)
+        favParkQuery = "INSERT INTO tFavoritePark (parkName, AddressID, image) VALUES (%s, LAST_INSERT_ID(), %s);"
+        data = (parkName, image)
+        cur.execute(favParkQuery, data)
+        db.commit()
+        favParkID = cur.insert_id()
+    #Wuery for favorite toy here... Combo box?
+    if (favToyID is None)
+        image = None
+        favToyQuery = "INSERT INTO tFavoriteToy (ToyName, image) VALUES (%s, %s);"
+        data = (ToyName, image)
+        cur.execute(favToyQuery, data)
+        db.commit()
+        favToyID = cur.insert_id()
+
+#finally commit all the dog data into the database.
+dogQuery = "Insert into tDog (breedID, gender, fixed, age, favParkID, favToyID, Size, Weight, bio, image"
+data = (breedID, gender, fixed, age, favParkID, favToyID, Size, weight, bio, image)
+cur.execute(dogQuery, data)
+db.commit()
+
 @app.route("/logout")
 @login_required
 def logout():
