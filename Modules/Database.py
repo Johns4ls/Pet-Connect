f973f9ad775e8329f84440467e6b9c8643e5b5b7
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2019-11-06 21:36:56 -0500

    match class

--- Modules/Database.py
+++ Modules/Database.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine import create_engine
 import sqlalchemy
 import pymysql
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.expression import ClauseElement
+from sqlalchemy import literal
 engine = sqlalchemy.create_engine(
     'mysql+pymysql://Website:W3bsite!@ec2-13-59-203-226.us-east-2.compute.amazonaws.com:3306/PetConnect',
     echo=True, pool_size=30)
@@ -203,3 +206,15 @@ session.query(tUser.firstName, '_score') \
 
 
 '''
+
+class Match(ClauseElement):
+    def __init__(self, columns, value):
+        self.columns = columns
+        self.value = literal(value)
+
+@compiles(Match)
+def _match(element, compiler, **kw):
+    return "MATCH (%s) AGAINST (%s)" % (
+               ", ".join(compiler.process(c, **kw) for c in element.columns),
+               compiler.process(element.value)
+             )
