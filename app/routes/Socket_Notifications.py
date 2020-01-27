971b7f94e4d5af765d9d200a8bfd3a5983ab987e
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2020-01-27 13:18:56 -0500

    Cleaned Notification implementation
    Cleaned up notification socket implementation

--- app/routes/Socket_Notifications.py
+++ app/routes/Socket_Notifications.py
@@ -3,32 +3,20 @@ import time
 from flask_socketio import SocketIO, emit, disconnect, test_client
 from app import socketio
 from flask_login import current_user
-from threading import Lock
 from flask import request
-notifyThread = None
-connected = True
-@socketio.on('askNotifications', namespace='/Notifications/')
-def asyncNotifications(msg):
-    global initial
-    initial = True
-    global connected 
-    connected = True
-    socketio.start_background_task(SendNotifications())
-def SendNotifications():
-    initial = True
-    global connected 
-    while(connected):
-        NewNotifications, count, notifications = Notifications.getNotifications(current_user.id)
-        if(count != 0 or initial is True):
-            for notification in notifications:
-                notification['ts'] = str(notification['ts'])
-            socketio.emit('getNotifications',
-                        {'NewNotifications': NewNotifications, 'count': count, 'notifications': notifications}, namespace='/Notifications/', room=request.sid) 
-            initial = False
-        socketio.sleep(2)
+
+@socketio.on('connect', namespace='/Notifications/')
+def connect():
+    return ''
+
+@socketio.on('Notifications', namespace='/Notifications/')
+def asyncNotifications():
+    NewNotifications, count, notifications = Notifications.getNotifications(current_user.id)
+        for notification in notifications:
+            notification['ts'] = str(notification['ts'])
+        socketio.emit('getNotifications',
+                    {'NewNotifications': NewNotifications, 'count': count, 'notifications': notifications}, namespace='/Notifications/', room=request.sid)
+
 @socketio.on('disconnect', namespace='/Notifications/')
-def test_disconnect():
+def disconnect():
     print('Client disconnected', request.sid)
-    disconnect()
-    global connected 
-    connected = False
