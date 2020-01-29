8c499dc823db4f6a3c17689b31f7c0dbd1ce1f11
Johns4ls <53236823+Johns4ls@users.noreply.github.com>
2020-01-28 21:32:29 -0500

    Fixed timestamps
    Fixed timestamps to now be correct when submitting to the database.

--- app/routes/Socket_AddAvailability.py
+++ app/routes/Socket_AddAvailability.py
@@ -8,13 +8,15 @@ from flask import request
 from Modules import Availability
 import json, ast
 from dateutil import parser
+from pytz import timezone
 @socketio.on('connect', namespace='/Availability/')
 def connect():
     print("connected")
 @socketio.on('getAvailability', namespace='/Availability/')
 def addAvailability(msg):
     msg = ast.literal_eval(json.dumps(msg))
-    Availability.commitAvailability(msg['dogID'], msg['userID'], parser.parse(msg['Begin_ts']), parser.parse(msg['End_ts']), msg['message'])
+    eastern = timezone('US/Eastern')
+    Availability.commitAvailability(msg['dogID'], msg['userID'], parser.parse(msg['Begin_ts']).astimezone(eastern), parser.parse(msg['End_ts']).astimezone(eastern), msg['message'])
 
 @socketio.on('askAvailability', namespace='/Availability/')
 def askAvailability(dogID):
@@ -27,7 +29,6 @@ def askAvailability(dogID):
     for time in Availability:
         time['Begin_ts'] = str(time['Begin_ts'])
         time['End_ts'] = str(time['End_ts'])
-    print(Availability)
     socketio.emit('giveAvailability',
                 {'Availability': Availability}, namespace='/Availability/', room=request.sid)
 
